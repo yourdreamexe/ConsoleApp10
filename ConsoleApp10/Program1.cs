ec481848ab96067a67063a72e911077cdcc8190e
yourdreamexe <krasher845@yandex.ru>
2023-11-19 00:00:30 +0300

    Добавили for, а так же ещё одну строчку ввода

--- ConsoleApp10/Program1.cs
+++ ConsoleApp10/Program1.cs
@@ -1,2 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Maksim");
+﻿int i = 1;
+for (i=0; i>1;i++ )
+{
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, Maksim");
+}
+Console.WriteLine("Hello, World!");
